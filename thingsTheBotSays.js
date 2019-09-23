45d63c45af99edc08c11cfed0a9d0c69801e6fbf
584973 <54097912+584973@users.noreply.github.com>
2019-09-23 17:40:32 +0200

    Update thingsTheBotSays.js

---
+++ thingsTheBotSays.js
@@ -0,0 +1,19 @@
+/*
+This JavaScript file has the robot's 'script', which is the list of questions or statements the robot will send as chat. 
+*/
+
+
+// botScript is a list of questions the robot will ask the user.
+var botScript = [
+    "Hello! What can i help you with?",
+    "Do you have the error code?",
+    "Hmm weird",
+    "Good luck with that, cuz i aint helpin you."
+  ];
+  
+  // goodbye is a variable that stores what the robot will say when it runs out of other things to say.
+  var goodbye = "Bye Bye :)";
+  
+  
+  
+  
